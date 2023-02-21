14e618270535f588f86a275f702951e965604925
Gyuhyun Lee <>
2023-02-21 10:03:21 -0800

    Adjusted entity weight

--- code/hb.cpp
+++ code/hb.cpp
@@ -199,7 +199,7 @@ GAME_UPDATE_AND_RENDER(update_and_render)
             add_pbd_vox_entity(game_state, &game_state->pbd_arena, 
                              game_state->loaded_voxs + game_state->loaded_vox_count,
                             V3d(-15, -15, 5), V3d(0, 0, 0),
-                            0.f, 1.0f/(random_between(&game_state->random_series, 50, 100)), color, 
+                            0.f, 1.0f/(random_between(&game_state->random_series, 800, 1000)), color, 
                             EntityFlag_Movable|EntityFlag_Collides);
             
             game_state->loaded_vox_count++;
